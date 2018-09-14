606ab25e3f4ca3259891deb67e2d71e99f24e002
RedSpecialL <rdspcll@gmail.com>
2018-09-14 23:30:25 +0300

    Added tracking bot as first simple ai enemy. Implemented movement logic for it.

---
+++ Source/CoopGameTLCourse/Private/AI/STrackerBot.cpp
@@ -0,0 +1,80 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "STrackerBot.h"
+
+#include "DrawDebugHelpers.h"
+#include "NavigationSystem.h"
+#include "NavigationPath.h"
+
+#include "Components/StaticMeshComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ASTrackerBot::ASTrackerBot()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(FName("MeshComp"));
+	MeshComp->SetCanEverAffectNavigation(false);
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp;
+
+	bUseVelocityChange = false;
+	MovementForce = 1000.0f;
+	RequiredDistanceToTarget = 100;
+}
+
+// Called when the game starts or when spawned
+void ASTrackerBot::BeginPlay()
+{
+	Super::BeginPlay();
+
+	NextPathPoint = GetNextPathPoint();
+}
+
+FVector ASTrackerBot::GetNextPathPoint()
+{
+	// Hack, to get player location.
+	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
+
+	UNavigationPath* NavPath = UNavigationSystemV1::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
+
+	if (NavPath->PathPoints.Num() > 1)
+	{
+		// Return next point in the path.
+		return NavPath->PathPoints[1];
+	}
+
+	// Failed to find path
+	return GetActorLocation();
+}
+
+// Called every frame
+void ASTrackerBot::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	float DistatnceToTarget = (GetActorLocation() - NextPathPoint).Size();
+
+	if (DistatnceToTarget <= RequiredDistanceToTarget)
+	{
+		NextPathPoint = GetNextPathPoint();
+
+		DrawDebugString(GetWorld(), GetActorLocation(), FString("TargetReached"));
+	}
+	else
+	{
+		//Keep moving towards next target.
+		FVector ForceDirection = NextPathPoint - GetActorLocation();
+		ForceDirection.Normalize();
+
+		ForceDirection *= MovementForce;
+
+		MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
+		DrawDebugDirectionalArrow(GetWorld(), GetActorLocation(), GetActorLocation() + ForceDirection, 32, FColor::Yellow, false, 0.0f, 0, 1.0f);
+	}
+
+	DrawDebugSphere(GetWorld(), NextPathPoint, 20, 12, FColor::Yellow, false, 0.0f, 1.0f);
+}
