3b6f5070a5fdcb28ebc2f7d851fe7ad57deb0e7a
RedSpecialL <rdspcll@gmail.com>
2018-09-20 22:38:54 +0300

    Started implementating of spawning bots wave logic.

--- Source/CoopGameTLCourse/Public/SGameMode.h
+++ Source/CoopGameTLCourse/Public/SGameMode.h
@@ -14,7 +14,37 @@ class COOPGAMETLCOURSE_API ASGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
+public:
+
+	ASGameMode();
+
+	virtual void StartPlay() override;
+
+protected:
+
+	FTimerHandle TimerHandle_BotSpawner;
+
+	/** Bots to spawn in current wave. */
+	int32 NumberOfBotsToSpawn;
+
+	int32 WaveCount;
+
+	UPROPERTY(EditDefaultsOnly, Category = "GameMode")
+	float TimeBetweenWaves;
+
+protected:
+
+	/** Hook for BP to spawn a single bot. */
+	UFUNCTION(BlueprintImplementableEvent, Category = "GameMode")
+	void SpawnNewBot();
 	
-	
-	
+	void SpawnBotTimerElapsed();
+
+	/** Starts spawning bots.*/
+	void StartWave();
+	/** Stops spawning bots.*/
+	void EndWave();
+	/** Sets timer for next startwave.*/
+	void PrepareForNextWave();
+
 };
